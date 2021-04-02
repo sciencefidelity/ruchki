2e404e2c96783087ebb4e7dc97b631055b7bbe90
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-02 11:58:33 +0000

    Update link and link styling

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Sciencefidelity!</a>
+          Welcome to <a href="https://sciencefidelity.co.uk">Sciencefidelity!</a>
         </h1>
 
         <p className={styles.description}>
