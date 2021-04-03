2a32f479379451ec95456606cb9e4226c96c7e99
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-03 11:38:31 +0000

    Change title text

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://sciencefidelity.co.uk">Sciencefidelity!</a>
+          Are you ready for <a href="https://sciencefidelity.co.uk">Sciencefidelity!</a>
         </h1>
 
         <p className={styles.description}>
