d52c88b4d96720a086dc9d2d5adbfd2d1f1d9b76
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-01-23 18:13:02 +0000

    refactor: prevent editors from creating two menus

--- studio/schemas/menu.ts
+++ studio/schemas/menu.ts
@@ -2,6 +2,7 @@ export default {
   name: 'menu',
   title: 'Menu',
   type: 'document',
+  __experimental_actions: [/*'create',*/ "update", /*'delete',*/ "publish"],
   fields: [
     {
       name: 'title',
